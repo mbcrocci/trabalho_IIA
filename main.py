7cb61bfcfdc7a86c620d7c33b7e4d434b1da5705
Maurizio Crocci <mbcrocci@gmail.com>
2016-01-09 14:26:05 +0000

    Encontra solucao

--- main.py
+++ main.py
@@ -1,96 +1,126 @@
 from sys import argv
+from collections import defaultdict
+import minimum_vertex_cover as mvc
 
 
-class Node:
-    def __init__(self, name):
-        self.name = name
-        self.parent = Node
-        self.childs = []
+class Graph:
+    """
+    Graph representa um grafo.
 
-    def __repr__(self):
-        string = "< Node: " + str(self.name)
-
-        if self.childs:
-            string += "  Childs:"
-            for child in self.childs:
-                string += " " + str(child)
-
-        string += " >"
-
-        return string
+    Este contem um dicionario de todos os nos e suas ligacoes:
+    Exemplo:
+        A : [B, C, D]
+        B : [D, A]
+        C : [D]
 
 
+    minimum vertex cover: https://en.wikipedia.org/wiki/K%C5%91nig%27s_theorem_(graph_theory)
 
-def exist_node(name, node_list):
-    for n in node_list:
-        if n.name == name:
-            return True, n
 
-    return False, Node(0)
+    """
+    def __init__(self, connections, directed=False):
+        self.graph = defaultdict(set)
+        self.directed = directed
+        self.add_connections(connections)
+
+    def add_connections(self, connections):
+        for parent, child in connections:
+            self.add(parent, child)
+
+    def add(self, parent, child):
+        self.graph[parent].add(child)
+        if not self.directed:
+            self.graph[child].add(parent)
+
+    def is_connected(self, node1, node2):
+        return node1 in self.graph and node2 in self.graph[node1]
+
+    def neighbours(self, id):
+        return self.graph[id]
+
+    def __str__(self):
+        return '{}({})'.format(self.__class__.__name__, dict(self.graph))
 
 
 def run():
     # open File
-
     try:
         file_name = argv[1]
 
     except IndexError:
         file_name = input("Enter a file name: ")
 
-    connections_list = []
-    connections_list, n_vertices, n_arestas = read_file(file_name, connections_list)
 
-    print(connections_list)
+    graph, n_vertices, n_arestas = read_file(file_name)
 
+    # MINIMUM VERTEX COVER
+    print("\nMVC")
 
-    node_list = create_node_list(connections_list)
+    # calcula o minimum vertex cover
+    m = mvc.min_vertex_cover(graph[0], graph[1])
+    print(m)
 
-    print("\n\n")
-    for node in node_list:
-        print(node)
+    """
+        Sendo A   - conjunto de todos os vertices
+              I   - conjunto de todos os vertices independentes
+              EC  - total vertex cover
+              MVC - Minimum vertex cover
 
+              A = I + EC
+              I = A - MVC
+    """
+    all_vertices = [i for i in range(1, n_vertices)] # (TODO): construir isto melhor
+    for v in m:
+        for u in m[v]:
+            try:
+                all_vertices.remove(u)
 
-def create_node_list(connections_list):
-    parents = []
-    childs = []
-    for c in connections_list:
-        if c[0] not in parents:
-            parents.append(c[0])
+            except ValueError:
+                pass
 
-        if c[1] not in childs:
-            childs.append(c[1])
-
-    # convert every parent to an Object
-    parent_nodes = [Node(p) for p in parents]
-
-    for p in parent_nodes:
-        for c in connections_list:
-            if c[0] == p.name:
-                p.childs.append(Node(c[1]))
-
-    return parent_nodes
+    print(all_vertices)
 
 
-def read_file(file_name, connection_list):
-    with open(file_name, 'r') as f:
-        n_vertices, n_arestas = 0, 0
 
-        for line in f.readlines():
-            if line.startswith('p'):
-                l = line.split(' ')
+def read_file(file_name):
+    """
+    :param file_name: nome do ficheiro a abrir
+    :return: left_v, right_v, n_vertices, n_arestas
+
+    Le o ficheiro extraindo o numero de vertices e numero de arestas
+    na linha que lhe corresponde (aquela que comeca por 'p')
+
+    Nas linhas que comecam por 'e', tira as ligacoes e cria 2 dicionarios, left_v e right_v.
+    left_v: dict em que as keys sao os vertices que se encontram a esquerda nas ligacoes
+            e o 'value' e uma lista de vertices que se encontram a direita nas ligacoes
+
+    right_v: mesmo que o left mas da direita para a esquerda
+    """
+
+    left_v = {} # dicionario que contem para cada vertice a esquerda da ligacao todos os que lhe correspondem a direita
+    right_v = {} # mesmo que left_v, mas da direita para a esquerda
+    with open(file_name, 'r') as graph:
+        for edge in graph:  # por cada linha
+            if edge.startswith('p'):
+                l = edge.split(' ')
                 n_arestas = int(l[2])
                 n_vertices = int(l[3])
 
-            if line.startswith('e'):
-                l = line.split(' ')
+            if edge.startswith('e'):
+                e = edge.split(' ')
+                try:
+                    left_v[int(e[1])].append(int(e[2]))
 
-                parent = int(l[1])
-                child = int(l[2])
+                except KeyError:
+                    left_v[int(e[1])] = [int(e[2])]
 
-                connection_list.append((parent, child))
+                try:
+                    right_v[int(e[2])].append(int(e[1]))
 
-    return connection_list, n_vertices, n_arestas
+                except KeyError:
+                    right_v[int(e[2])] = [int(e[1])]
+
+    return [left_v, right_v], n_vertices, n_arestas
 
 if __name__ == '__main__':
     run()
